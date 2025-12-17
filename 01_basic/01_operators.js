b2131a95676df8a6e46552b2eac92f40c165e2a7
deb1804 <129526311+deb1804@users.noreply.github.com>
2025-12-17 18:55:40 +0000

    datatypes

---
+++ 01_basic/01_operators.js
@@ -0,0 +1,23 @@
+"use strict"; //treat alljs code as newe version
+// alert(3+3); we are using node js not a browser.
+console.log(3+3); console.log("Dev ");//but this is not readable bad way of presenting
+let name="Dev";//this is a string datatype
+let age=18;
+let isLoggedIn=true; //boolean type
+/*
+  number 2 to the power 53
+  bigint 
+  string=" "
+  boolean = T/F?
+  null it is atandAlone value it is a type
+  undefined=value is not assigned but var is declared.
+  symbol =to determine uniqness in something
+  object 
+*/
+console.log(typeof "Dev");
+console.log(age);
+console.log(null);//something fishy Object null is an object iin js
+console.log(undefined);
+
+
+
