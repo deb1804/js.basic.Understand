482b20cff202103addacecba7dbaf6eca6644355
deb1804 <129526311+deb1804@users.noreply.github.com>
2025-12-18 06:31:59 +0000

    op

--- 01_basic/operations.js
+++ 01_basic/operations.js
@@ -2,6 +2,18 @@ console.log("1"+2)
 console.log(2+"1")
 console.log("1"+2+2)
 console.log(1+2+"2") //suprising na
+console.log(+true) //Boolean operation
+// console.log(true+). //here it is not validating the valuess
+console.log(+"");
+let num1,num2,num3;
+num1=num2=num3=2+2;
+console.table([num1,num2,num3])
+
+let gameCounter=100
+gameCounter++       //postfix operation
+console.log(gameCounter) 
+
+//go through cdn prefix and postfix operatons for better understandings.
 
 
 
